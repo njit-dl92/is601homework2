898461eda80b3e065165eb3973f9abc638ee950f
Dinesh Lankepillewar <dl92@njit.edu>
2024-09-15 17:05:00 -0400

    Modified calculator fuctions as well as tests for it

---
+++ projectSetup/calculator/__init__.py
@@ -0,0 +1,31 @@
+"""
+This is the calculator module.
+It provides basic mathematical operations like addition.
+"""
+
+def add(a, b):
+    """
+    Adds two numbers together.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The sum of the two numbers.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Subtract two numbers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The difference of the two numbers.
+    """
+    return a - b
+    
